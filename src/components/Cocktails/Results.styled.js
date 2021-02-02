90e16f8b0b38ac207a9f87d5fd1f1d3ac1f809c7
Paul Shorey <ps@artspaces.net>
2021-02-01 23:50:41 -0500

    responsive columns

--- src/components/Cocktails/Results.styled.js
+++ src/components/Cocktails/Results.styled.js
@@ -3,8 +3,14 @@ import styled from "styled-components";
 export default styled.article`
   padding: 0.75rem 0;
   overflow: auto;
-  column-count: 3;
   column-fill: balance;
+  column-count: 3;
+  @media (max-width:1100px) {
+    column-count: 2;
+  }
+  @media (max-width:650px) {
+    column-count: 1;
+  }
   > * {
     break-inside: avoid;
     margin-bottom: 0.75rem;
