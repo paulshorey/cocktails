a786056d6a6d2ebbcb35a14c94b60ce845cf4a37
Paul Shorey <ps@artspaces.net>
2021-02-09 23:48:44 -0500

    tests work-in-progress

--- README.md
+++ README.md
@@ -12,20 +12,24 @@
 - Uses an unusual architecture for one of the components. See **`src/components/Cocktails`**. [ABOUT.md](src/components/Cocktails/ABOUT.md)
 - Data is imported from a SQL backup, into a purely Front-End JavaScript "in-browser database" [alasql](https://github.com/agershun/alasql). You can query it using standard SQL. See the abstraction in: **`src/data/cocktails.js`** line 100.
 
-## Testing:
+## Testing :
 
-This uses a new end-to-end testing technique, run by Jest + Pupeeteer. It's more useful (compared to traditional unit tests) for more complex projects like my https://dogs.paulshorey.com, which gets a lot of data from many API data, and server-side-renders them into dynamic route urls. However, I thought it would be beneficial to include it here as well. End-to-end tests are very fast to write and have the benefit of testing the app exactly as the visitor will see it.
+This project uses NextJS infrastructure and a lot of server-side-generated content. Unit tests would be a big pain to set up. What's much easier and actually more effective? Try "end-to-end testing", also called "functional testing". Use "Puppeteer" (a programmable Chrome browser) to check that all important content has been generated correctly and all important user-interactions are working.
 
-### Do **`npm run test`** when adding/editing tests
+> No more spending hours mocking data sources. Nothing to mock at all. It's real - same content the user will see. This "puppeteer" powered "functional testing" runs just as reliably and almost as quickly as traditional unit tests.
+
+### npm run test
 
 **When developing the app, just `git commit` the changes.** Testing is integrated into the CI process. The tests will automatically run. If the tests fail, the commit will also fail.
 
-1. First, the script runs **lint**, **build**, then starts serving the app at port **:9754**.
-2. Then it runs tests using `jest --verbose`. Jest can be configured in `jest.config.js`
-3. Whether tests succeed or fail, the ":9754" server is stopped, so it be started again next time.
-4. If any tests fail (including the initial `lint`), you will see instructions about what needs to be fixed. If this was triggered by a `git commit`, it will not be allowed to execute until you fix the tests and run `git commit` again.
+> Step 1) Runs **lint**, **build**, and **serve** the app.
+> Step 2) Runs tests using `jest --verbose`.
+> Step 3) Whether tests succeed or fail, the server is stopped, so it will not block future processes.
+> Step 4) If any tests fail (including the initial `lint`), you will see instructions about what needs to be fixed. If this was triggered by a `git commit`, you will not be allowed to push changes until you fix the tests and run `git commit` again.
 
-**This uses "husky" to add "pre-commit" or other hooks:**
+### Pre-commit hook:
+
+This uses "husky" to add "pre-commit" or other hooks to your "git commit" command:
 
 ```
 "husky": {
@@ -33,13 +37,4 @@ This uses a new end-to-end testing technique, run by Jest + Pupeeteer. It's more
     "pre-commit": "npm run test"
   }
 },
-"test": "npm run lint && npm run build && npm run test_start_server && npm run test_run_tests && npm run test_stop_server",
-```
-
-Helper scripts used by `npm run test`:
-
-```
-"test_start_server": "next start -p 9754 &",
-"test_run_tests": "jest --verbose || npm run test_stop_server",
-"test_stop_server": "kill -9 $(lsof -i TCP:9754 | grep LISTEN | awk '{print $2}')"
 ```
--- tests/page_home.js
+++ tests/page_home.js
@@ -1,82 +1,132 @@
-describe("Cocktails", () => {
-  let innerText = "";
+const fsOriginal = require("fs");
+const fs = fsOriginal.promises;
+const SELF_DIR = "tests/";
+const TMP_DIR = SELF_DIR + "screenshots";
+fs_setup();
+const usePort = process.env.USE_PORT || "3004";
 
+/*
+ * Test
+ */
+describe("Cocktails page", () => {
+  /*
+   * Setup
+   */
+  let sn = 0; // screenshot number
+  let innerText = ""; // update innerText after loading new page
   beforeAll(async () => {
-    await page.goto("http://localhost:9754/");
+    await page.goto(`http://localhost:${usePort}/`);
     innerText = await page.evaluate(() => document.body.textContent);
+    // debug screenshot
+    await page.screenshot({ path: SELF_DIR + `screenshots/${sn++} initial load.png` });
   });
 
   /*
-   * DEFAULT STATE is {tags: {"Orange juice":"strIngredients"}}
+   * Crawl
    */
-
-  it('on first load, page should be filtering by "Orange juice"', async () => {
-    let title = await page.evaluate(() => document.querySelector(".Result:nth-child(4) .title").innerText);
-    await expect(title).toBe("Orange Oasis");
-  });
-
-  it(`un-check "Orange juice" filter by clicking dropdown checkbox`, async () => {
-    // click to see dropdown
-    let focusOn = await page.evaluate(() =>
-      document.querySelector("#filterByIngredients .label-input")
-    );
-    if (focusOn) {
-      await focusOn.click();
-    }
-    // click to select option
-    let clickOn = await page.evaluate(() =>
-      document.querySelector("#filterByIngredients .options-dropdown .option:nth-child(4) input")
-    );
-    if (clickOn) {
-      await clickOn.click();
-    }
-    // test
-    let title = await page.evaluate(() => document.querySelector(".Result:nth-child(4) .title").innerText);
-    await expect(title).toBe("Negroni");
-  });
-
-  it(`filter by ingredient "Orange juice" again`, async () => {
-    let clickOn = await page.evaluate(() => document.querySelector("#filterBy .options-dropdown .option:nth-child(4) input"));
-    if (clickOn) {
-      await clickOn.click();
-    }
-    let title = await page.evaluate(() => document.querySelector(".Result:nth-child(4) .title").innerText);
-    await expect(title).toBe("Orange Oasis");
-  });
-
-  it(`un-check "Orange juice" filter by clicking its Tag "X"`, async () => {
-    let clickOn = await page.evaluate(() => document.querySelector("#filterBy .tags .Tag"));
-    if (clickOn) {
-      await clickOn.click();
-    }
-    let title = await page.evaluate(() => document.querySelector(".Result:nth-child(4) .title").innerText);
-    await expect(title).toBe("Negroni");
-  });
-
-  it(`sort by name first click: ascending`, async () => {
-    const [clickOn] = await page.$x(`//*[@id="sortBy"]/*[contains(., 'name')]`);
-    if (clickOn) {
-      await clickOn.click();
-    }
-    let title = await page.evaluate(() => document.querySelector(".Result:nth-child(4) .title").innerText);
-    await expect(title).toBe("Apello");
-  });
-
-  it(`sort by name second click: descending`, async () => {
-    const [clickOn] = await page.$x(`//*[@id="sortBy"]/*[contains(., 'name')]`);
-    if (clickOn) {
-      await clickOn.click();
-    }
-    let title = await page.evaluate(() => document.querySelector(".Result:nth-child(4) .title").innerText);
-    await expect(title).toBe("Thai Iced Coffee");
-  });
-
-  it(`sort by name third click: back to unsorted`, async () => {
-    const [clickOn] = await page.$x(`//*[@id="sortBy"]/*[contains(., 'name')]`);
-    if (clickOn) {
-      await clickOn.click();
-    }
-    let title = await page.evaluate(() => document.querySelector(".Result:nth-child(4) .title").innerText);
-    await expect(title).toBe("Bora Bora");
-  });
+  {
+    let test = it('on first load, page should be filtering by "Orange juice"', async () => {
+      // DEFAULT STATE IS {tags: {"Orange juice":"strIngredients"}}
+      let title = await page.evaluate(() => document.querySelector(".Result:nth-child(4) .title").innerText);
+      await expect(title).toBe("Orange Oasis");
+      // debug screenshot
+      await page.screenshot({ path: SELF_DIR + `screenshots/${sn++} ${test.description}.png` });
+    });
+  }
+  {
+    let test = it(`un-check "Orange juice" filter by clicking dropdown checkbox`, async () => {
+      // interact
+      await page.click('#filterByIngredients .options-dropdown .option:nth-child(4) input');
+      // test
+      let title = await page.evaluate(() => document.querySelector(".Result:nth-child(4) .title").innerText);
+      await expect(title).toBe("Negroni");
+      // debug screenshot
+      await page.screenshot({ path: SELF_DIR + `screenshots/${sn++} ${test.description}.png` });
+    });
+  }
+  {
+    let test = it(`filter by ingredient "Orange juice" again`, async () => {
+      // interact
+      await page.click('#filterByIngredients .options-dropdown .option:nth-child(4) input');
+      // test
+      let title = await page.evaluate(() => document.querySelector(".Result:nth-child(4) .title").innerText);
+      await expect(title).toBe("Orange Oasis");
+    });
+  }
+  {
+    let test = it(`un-check "Orange juice" filter by clicking its Tag "X"`, async () => {
+      // interact
+      await page.click('#filterBy .tags .Tag');
+      // test
+      let title = await page.evaluate(() => document.querySelector(".Result:nth-child(4) .title").innerText);
+      await expect(title).toBe("Negroni");
+      // debug screenshot
+      await page.screenshot({ path: SELF_DIR + `screenshots/${sn++} ${test.description}.png` });
+    });
+  }
+  {
+    let test = it(`sort by name first click: ascending`, async () => {
+      const [clickOn] = await page.$x(`//*[@id="sortBy"]/*[contains(., 'name')]`);
+      if (clickOn) {
+        await clickOn.click();
+      }
+      let title = await page.evaluate(() => document.querySelector(".Result:nth-child(4) .title").innerText);
+      await expect(title).toBe("Apello");
+      // debug screenshot
+      await page.screenshot({ path: SELF_DIR + `screenshots/${sn++} ${test.description}.png` });
+    });
+  }
+  {
+    let test = it(`sort by name second click: descending`, async () => {
+      const [clickOn] = await page.$x(`//*[@id="sortBy"]/*[contains(., 'name')]`);
+      if (clickOn) {
+        await clickOn.click();
+      }
+      let title = await page.evaluate(() => document.querySelector(".Result:nth-child(4) .title").innerText);
+      await expect(title).toBe("Thai Iced Coffee");
+      // debug screenshot
+      await page.screenshot({ path: SELF_DIR + `screenshots/${sn++} ${test.description}.png` });
+    });
+  }
+  {
+    let test = it(`sort by name third click: back to unsorted`, async () => {
+      const [clickOn] = await page.$x(`//*[@id="sortBy"]/*[contains(., 'name')]`);
+      if (clickOn) {
+        await clickOn.click();
+      }
+      let title = await page.evaluate(() => document.querySelector(".Result:nth-child(4) .title").innerText);
+      await expect(title).toBe("Bora Bora");
+      // debug screenshot
+      await page.screenshot({ path: SELF_DIR + `screenshots/${sn++} ${test.description}.png` });
+    });
+  }
 });
+
+
+/*
+ * LIB
+ */
+async function click_and_wait(page, selector) {
+  return new Promise(async (resolve) => {
+    try {
+      let response = await Promise.all([
+        page.waitForNavigation(), // The promise resolves after navigation has finished
+        page.click(selector) // Clicking the link will indirectly cause a navigation
+      ]);
+      resolve(response);
+    } catch (e) {
+      console.error(e);
+      resolve(false);
+    }
+  });
+}
+
+async function fs_setup() {
+  await fs.rmdir(TMP_DIR, { recursive: true }, (err) => {
+    if (err) {
+      throw err;
+    }
+    console.log(`rm ${TMP_DIR}`);
+  });
+  await fs.mkdir(TMP_DIR);
+}
